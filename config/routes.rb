54f645ec47ca8051d1dfd74e6a6f1f06af989b7e
None <akira.tamai.main@gmail.com>
2015-09-26 06:26:15 +0000

    add top

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   get 'top/index'
 
-  get 'type/index'
-
+  post 'type/index' => 'type#index'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
