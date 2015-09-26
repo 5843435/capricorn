f6866b9f61095377b4b7ad6dcd5ef4ddd4df706c
AkinobuKatou <e5843435@yahoo.co.jp>
2015-09-26 02:43:36 +0000

    production.rb modified

--- app/controllers/type_controller.rb
+++ app/controllers/type_controller.rb
@@ -1,29 +1,30 @@
 class TypeController < ApplicationController
   def index
 #    @type = params[:type]
-    @type = 1
+    @type = 2 
     case @type
-    when @type == "1"
+    when 1
     @text = "タイプ1でーす"
-    when @type == "2"
+    when 2
     @text = "タイプ2でーす"
-    when @type == "3"
+    when 3
     @text = "タイプ3でーす"
-    when @type == "4"
+    when 4
     @text = "タイプ4でーす"
-    when @type == "5"
+    when 5
     @text = "タイプ5でーす"
-    when @type == "6"
+    when 6
     @text = "タイプ6でーす"
-    when @type == "7"
+    when 7
     @text = "タイプ7でーす"
-    when @type == "8"
+    when 8
     @text = "タイプ8でーす"
-    when @type == "9"
+    when 9
     @text = "タイプ9でーす"
-    when @type == "10"
+    when 10
     @text = "タイプ10でーす"
     else
     @text = "タイプ11でーす"
+    end
   end
 end
