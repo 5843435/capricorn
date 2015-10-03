d7d84e051cb5e2b1ffe8b1b6a17fd8ba6c8ad37a
AkinobuKatou <e5843435@yahoo.co.jp>
2015-10-03 15:39:59 +0000

    devise on Japanese

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,6 +37,17 @@ Rails.application.configure do
   config.assets.raise_runtime_errors = true
 
   config.action_mailer.default_url_options = { host: 'e5843436.cloudapp.net' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    user_name: 'akinobu.katou@gmail.com',
+    password: 'Jpn12008',
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
