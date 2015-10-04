66e46f0b2a6e1ecc7413e1c7efd0d8a43f2be3a4
vagrant <kam.kaz.113@gmail.com>
2015-10-04 15:25:02 +0000

    edit type controller.

--- app/controllers/type_controller.rb
+++ app/controllers/type_controller.rb
@@ -9,7 +9,12 @@ class TypeController < ApplicationController
     #男性の1日の平均使用量(メートル)からあと何日で在庫がなくなるか計算する
     man_consume_oneday = 3.5
     woman_consume_oneday = 12.5
-    consume_oneday = (current_user.family_men.to_i * man_consume_oneday) + (current_user.family_women * woman_consume_oneday)
+    consume_oneday = 0
+    if current_user.family_men.to_i > 0 && current_user.family_women.to_i > 0 then
+        consume_oneday = (current_user.family_men.to_i * man_consume_oneday) + (current_user.family_women * woman_consume_oneday)
+    else
+        consume_oneday = man_consume_oneday
+    end
     @days  = (params[:stock].to_i/consume_oneday)*(60)
     @consume_oneday = consume_oneday
     #楽天ブックスAPIによるデータ取得
