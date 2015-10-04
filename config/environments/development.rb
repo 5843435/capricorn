e0d2aa21b398c534f0efa35f4097b5dbe2c16da2
AkinobuKatou <e5843435@yahoo.co.jp>
2015-10-04 06:13:56 +0000

    ユーザ登録でメール認証をいったん不要に変更

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,16 +37,12 @@ Rails.application.configure do
   config.assets.raise_runtime_errors = true
 
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.smtp_settings = {
-    address: 'smtp.gmail.com',
-    port: 587,
-    user_name: 'username@gmail.com',
-    password: 'password',
-    authentication: :plain,
-    enable_starttls_auto: true
-  }
+  #config.action_mailer.delivery_method = :smtp
+  #config.action_mailer.raise_delivery_errors = true
+  #config.action_mailer.smtp_settings = {
+  #  address: 'e5843436.cloudapp.net',
+  #  port: 25,
+  #}
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
