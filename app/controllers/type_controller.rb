fd1454b09cd03419524aa1969ca8592daff700ac
vagrant <kam.kaz.113@gmail.com>
2015-10-05 15:13:56 +0000

    change consume logic

--- app/controllers/type_controller.rb
+++ app/controllers/type_controller.rb
@@ -9,13 +9,24 @@ class TypeController < ApplicationController
     #男性の1日の平均使用量(メートル)からあと何日で在庫がなくなるか計算する
     man_consume_oneday = 3.5
     woman_consume_oneday = 12.5
-    consume_oneday = 0
-    if current_user.family_men.to_i > 0 && current_user.family_women.to_i > 0 then
-        consume_oneday = (current_user.family_men.to_i * man_consume_oneday) + (current_user.family_women * woman_consume_oneday)
-    else
-        consume_oneday = man_consume_oneday
+    consume_oneday = man_consume_oneday
+    if !current_user.nil? then
+        #if current_user.family_men.to_i > 0 && current_user.family_women.to_i > 0 then
+            #1日の消費量計算
+            consume_oneday = (current_user.family_men.to_i * man_consume_oneday) + (current_user.family_women * woman_consume_oneday)
+            if !current_user.prefecture.index("沖縄").nil? then
+                consume_oneday = consume_oneday*2
+            end
+            if !current_user.prefecture.index("大分").nil? then
+                consume_oneday = consume_oneday/2
+            end
+        #end
     end
-    @days  = (params[:stock].to_i/consume_oneday)*(60)
+    #終了日計算
+    now = Time.now
+    days  = (params[:stock].to_i/consume_oneday)*(30)
+    @end_day = now + (days.floor * 24 * 3600)
+    @days = days.floor
     @consume_oneday = consume_oneday
     #楽天ブックスAPIによるデータ取得
 =begin
