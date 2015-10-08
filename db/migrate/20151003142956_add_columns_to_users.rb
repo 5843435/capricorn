5d86c0150603fbfeb244e44ba80ffc266555c509
AkinobuKatou <e5843435@yahoo.co.jp>
2015-10-08 11:47:28 +0000

    refactoring migrate file

--- db/migrate/20151003142956_add_columns_to_users.rb
+++ db/migrate/20151003142956_add_columns_to_users.rb
@@ -1,7 +1,7 @@
 class AddColumnsToUsers < ActiveRecord::Migration
   def change
     add_column :users, :name, :string
-    add_column :users, :birthday, :string
+    add_column :users, :birthday, :date
     add_column :users, :sex, :integer
     add_column :users, :family_men, :integer
     add_column :users, :family_women, :integer
