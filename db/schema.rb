40b8a6f467cd7d3e9e93780b91c16ec07654113b
Junki <the.reticent.snow@gmail.com>
2015-10-24 05:31:14 +0000

    fixed image size of TOP page

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20151008121401) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "items", force: :cascade do |t|
     t.string   "name"
     t.integer  "spent_men"
@@ -60,4 +57,5 @@ ActiveRecord::Schema.define(version: 20151008121401) do
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+
 end
