e6fbe1888c85f11d908ec9e11b0dc5d0839322d4
Junki <the.reticent.snow@gmail.com>
2015-10-24 05:38:35 +0000

    modify schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20151008121401) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "items", force: :cascade do |t|
     t.string   "name"
     t.integer  "spent_men"
