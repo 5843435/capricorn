eb1d808f9a4c15f8fbbd90801dd4fcd3bc17526f
None <akira.tamai.main@gmail.com>
2015-11-06 14:21:34 +0000

    fix branch

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151103030650) do
+ActiveRecord::Schema.define(version: 20151103061117) do
 
   create_table "items", force: :cascade do |t|
     t.string   "name"
