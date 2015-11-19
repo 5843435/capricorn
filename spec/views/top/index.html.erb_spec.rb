0e2f365b1140d60f3f929d3b7f6fd7a1e39a6085
None <akira.tamai.main@gmail.com>
2015-11-19 04:17:47 +0000

    fix top and test for travis

--- spec/views/top/index.html.erb_spec.rb
+++ spec/views/top/index.html.erb_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 describe 'トップページ' do
-  specify 'capricorn表示' do
+  specify 'ずぼらパンダ表示' do
     visit root_path
-    expect(page).to have_css('h1', text: 'Capricorn')
+    expect(page).to have_css('h1', text: 'ずぼらパンダ')
   end
 end
