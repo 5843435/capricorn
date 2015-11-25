ff0ed0ef4bb56ecc6bac6fedb7486c36c1bf96f7
None <akira.tamai.main@gmail.com>
2015-11-25 14:13:15 +0000

    test memo

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -154,6 +154,12 @@ class StocksController < ApplicationController
     #end
   end
 
+  # マックスチャージ
+  def cherge_charge
+    copy = @stock.clone
+    copy.save
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_stock
